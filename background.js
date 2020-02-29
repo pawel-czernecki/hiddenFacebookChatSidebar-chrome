66e534c426d2724a611ff773e96303147d2631d0
Paweł Czernecki <czerneckip1@gmail.com>
2020-02-29 09:28:23 +0100

    added AutoHideSidebar

---
+++ background.js
@@ -0,0 +1,5 @@
+document.getElementsByTagName( 'html' )[0].classList.remove("sidebarMode");
+
+window.addEventListener('resize', ()=>{
+    document.getElementsByTagName( 'html' )[0].classList.remove("sidebarMode");
+});
